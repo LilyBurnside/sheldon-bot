bb2e5d64b6168b6d933e2547c7108f92ac17ef9c
LilyBurnside <lillian.burnside@gmail.com>
2021-09-01 09:56:42 -0500

    added communism

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
+const module = require('./communist.js');
+const communistManifesto = module.communistManifesto;
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on("message", msg => {
-  if (msg.content) {
+  if (msg.content.includes('lol') || msg.content.includes('haha')) {
     msg.reply("Bazinga!");
+  }
+
+  if (msg.content.includes('!communist')) {
+    msg.reply(communistManifesto[0])
   }
 })
 
